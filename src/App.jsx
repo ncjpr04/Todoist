a3f0c60b9cec14994894a27207856305c75a7eed
Nitin Choudhary <nitinchoudhary22112004@gmail.com>
2024-06-02 16:32:44 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Todo from './components/Todo.jsx'
+import { Provider } from 'react-redux'
+import {store} from "./app/store"
+function App() {
+
+  return (
+    <>
+      <Provider store={store}>
+        <Todo />
+      </Provider>
+    </>
+  )
+}
+
+export default App
